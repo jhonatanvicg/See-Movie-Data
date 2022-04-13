8bb7a13635ddd541575f70f8bc6e0d52d4894df5
Jhonatan Victorio <jhonatanvicg@gmail.com>
2022-04-13 15:06:02 -0500

    Agregando modal a las ventanas de FAVORITOS y BUSQUEDA

--- pages/Favorites.js
+++ pages/Favorites.js
@@ -1,31 +1,36 @@
 import { useContext, useState } from "react";
 import RowItem from "../components/RowItem";
 import AppContext from "../context/AppContext";
+import Modal from "../components/Modal";
 
 
 const Favorites = () => {
 
   const [localMovies,setLocalMovies] = useState()
+
   const { favoriteMovies } = useContext(AppContext);
 
 
   return ( 
-    <div className="Favorites">
-      {
-        favoriteMovies.movieList.length == 0 ?
+    <>
+      <Modal/>
+      <div className="Favorites">
+        {
+          favoriteMovies.movieList.length == 0 ?
 
-        <div className="Favorites__Empty">
-          <img src="/images/money.png" alt="" />
-          <p>Your List is Empty</p>
-        </div>
+          <div className="Favorites__Empty">
+            <img src="/images/money.png" alt="" />
+            <p>Your List is Empty</p>
+          </div>
 
-        :
+          :
 
-        favoriteMovies.movieList.map((movie)=>(
-          <RowItem movie={movie} Topic="Favorite--Movie__Item" />
-        ))
-      }
-    </div>
+          favoriteMovies.movieList.map((movie)=>(
+            <RowItem movie={movie} Topic="Favorite--Movie__Item" />
+          ))
+        }
+      </div>
+    </>
    );
 }
  
--- pages/Search.js
+++ pages/Search.js
@@ -1,25 +1,29 @@
 import { useContext } from "react";
 import RowItem from "../components/RowItem";
 import AppContext from "../context/AppContext";
+import Modal from "../components/Modal";
 
 const Search = () => {
   
   const { listSearch } = useContext(AppContext)
   
   return ( 
-    <div className="Search">
-      {
-        listSearch.length == 0 ?
-          <div className="Favorites__Empty">
-            <img src="/images/money.png" alt="" />
-            <p>Search Not Found</p>
-          </div>
-        :
-          listSearch.map((movie)=>(
-            <RowItem movie={movie} Topic="Favorite--Movie__Item" />
-          ))
-      }
-    </div>
+    <>
+      <Modal />
+      <div className="Search">
+        {
+          listSearch.length == 0 ?
+            <div className="Favorites__Empty">
+              <img src="/images/money.png" alt="" />
+              <p>Search Not Found</p>
+            </div>
+          :
+            listSearch.map((movie)=>(
+              <RowItem movie={movie} Topic="Favorite--Movie__Item" />
+            ))
+        }
+      </div>
+    </>
    );
 }
  
